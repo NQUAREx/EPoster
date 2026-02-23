2d4797481fea5b52112cd6786ef33376ef311d23
NQUARE <100482393+NQUAREx@users.noreply.github.com>
2026-02-24 02:47:00 +0300

    Adjust base state blob size and count range

--- ui/app.js
+++ ui/app.js
@@ -40,8 +40,8 @@ const lavaRuntime = {
 };
 
 const LAVA_CONFIG = {
-  minBlobs: 15,
-  maxBlobs: 16,
+  minBlobs: 10,
+  maxBlobs: 20,
   minLifeMs: 60000,
   maxLifeMs: 300000,
   anomalyChance: 0.1,
@@ -189,7 +189,7 @@ class LavaBlob {
     this.container = container;
 
     this.isAnomaly = Math.random() < LAVA_CONFIG.anomalyChance;
-    this.size = 20 + (Math.random() * 24.55);
+    this.size = 20 + (Math.random() * 13.4125);
     this.el.style.width = `${this.size}vw`;
     this.el.style.height = `${this.size}vw`;
 
